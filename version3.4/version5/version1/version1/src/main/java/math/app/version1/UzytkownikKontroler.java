5555ad21a16392f50e8533fde6bc320244b3d81e
Quartozane <73704941+Quartozane@users.noreply.github.com>
2023-05-07 21:57:52 +0200

    Add files via upload

--- version3.4/version5/version1/version1/src/main/java/math/app/version1/UzytkownikKontroler.java
+++ version3.4/version5/version1/version1/src/main/java/math/app/version1/UzytkownikKontroler.java
@@ -9,25 +9,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UzytkownikKontroler {
     @Autowired
     private UzytkownikRepozytorium uzytkownikRepozytorium;
+
     @GetMapping(path = "/signin")
     public String getLog() {
         return "zaloguj";
     }
+
     @PostMapping(path = "/signin")
-    @ResponseBody
-    public ResponseEntity<Uzytkownik> ZatwierdzLogowanie(@RequestParam @Email String email, @RequestParam @Size(min = 8) String haslo) {
+    public ModelAndView ZatwierdzLogowanie(@RequestParam @Email String email, @RequestParam @Size(min = 8) String haslo) {
         Uzytkownik user = uzytkownikRepozytorium.findByEmailAndHaslo(email, haslo);
         if (user != null) {
             user.setHaslo("********");
-                return ResponseEntity.ok(user);
-            }
-            else {
-            return ResponseEntity.notFound().build();
+            ModelAndView modelAndView = new ModelAndView("redirect:/wybierz-dzial");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("zaloguj");
+            modelAndView.addObject("error", "Nieprawidłowy email lub hasło.");
+            return modelAndView;
         }
     }
+    //JSON
+    /*
+
+     */
 }
